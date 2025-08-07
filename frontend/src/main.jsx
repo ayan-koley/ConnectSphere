e700bfb1ad2189f5bad5beefeac76eca59704ba1
Ayan Koley <koleyayan07@gmail.com>
2025-08-07 18:16:28 +0530

    feat: remove afterSignOutUrl and signUpFallbackRedirectUrl from ClerkProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 import store from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
-    <ClerkProvider publishableKey='pk_test_YWRhcHRlZC1zd2FuLTQxLmNsZXJrLmFjY291bnRzLmRldiQ' afterSignOutUrl={"/"} signUpFallbackRedirectUrl={"/"} >
+    <ClerkProvider publishableKey='pk_test_YWRhcHRlZC1zd2FuLTQxLmNsZXJrLmFjY291bnRzLmRldiQ'>
       <Provider store={store}>
         <RouterProvider router={router} />
       </Provider>
